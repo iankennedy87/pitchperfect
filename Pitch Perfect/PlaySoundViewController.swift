be8ed93c255917dacc143b5b3c500e8a07c77596
Ian Kennedy <iankennedy87@gmail.com>
2015-10-24 13:32:20 -0700

    Change stopAudio

--- Pitch Perfect/PlaySoundViewController.swift
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -52,8 +52,7 @@ class PlaySoundViewController: UIViewController {
     }
     
     @IBAction func stopAudio(sender: UIButton) {
-        audioPlayer.stop()
-        audioPlayerNode.stop()
+        resetAudio()
     }
     
     func resetAudio() {
